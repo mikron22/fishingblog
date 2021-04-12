a29edd2b96e798828848a6aa7059ad0d0ce6ccf1
mikron22 <mikron2222@gmail.com>
2021-04-12 14:18:18 +0200

    Change database to postgres

--- fishingblog/settings.py
+++ fishingblog/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'fishingblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dapoid7j26k04r',
+        'USER': 'qqqskfcjkeuuwd',
+        'HOST': 'ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
