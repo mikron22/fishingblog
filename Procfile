f2be1cde888d77868370e1f19017141fc67e854c
mikron22 <mikron2222@gmail.com>
2021-04-12 13:19:35 +0200

    Prepare for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jsfishingblog.wsgi --log-file -
--- fishingblog/urls.py
+++ fishingblog/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 from blog import urls
-from fishingblog.settings import DEBUG, MEDIA_ROOT, MEDIA_URL
+from fishingblog.settings import DEBUG, MEDIA_ROOT, MEDIA_URL, STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', include(urls))
+    path('', include(urls)),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
