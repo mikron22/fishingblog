e1a9e372f6f9798bf1a442a1ba2143519a41730c
mikron22 <mikron2222@gmail.com>
2021-04-19 18:45:32 +0200

    Update plurals in admin

--- blog/models.py
+++ blog/models.py
@@ -13,25 +13,37 @@ class Post(models.Model):
     image = ResizedImageField(size=[800, 800], upload_to='images/', force_format="jpeg", verbose_name="Zdjęcie (opcjonalne)", null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "Wpis"
+
     class Meta:
         verbose_name = 'Wpisy'
+        verbose_name_plural = 'Wpisy'
 
 class Comment(models.Model):
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Komentarz"
     
     class Meta:
         ordering = ["-created_at"]
         verbose_name = 'Komentarze'
+        verbose_name_plural = 'Komentarze'
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = ResizedImageField(size=[240, 240], null=True, upload_to='images/', force_format="jpeg", default="default.jpg")
 
+    def __str__(self):
+        return "Profil"
+
     class Meta:
         verbose_name = "Profile użytkownika"
+        verbose_name_plural = "Profile użytkownika"
 
 
 @receiver(post_save, sender=User)
